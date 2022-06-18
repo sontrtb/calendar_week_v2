5bbc1eccab4d62bf54c013554e851621a1a37753
unknown <sontrtb@gmail.com>
2022-06-19 00:59:20 +0700

    login

--- src/config/connfig.js
+++ src/config/connfig.js
@@ -1,7 +1,8 @@
-// const API = "https://calendar-week-be.herokuapp.com/api/v1";
+const API = "https://calendar-week-be.herokuapp.com/api/calendar";
+const API_ACCOUNT = "https://calendar-week-be.herokuapp.com/api/account";
 
-const API = "http://localhost:3001/api/calendar";
-const API_ACCOUNT = "http://localhost:3001/api/account";
+// const API = "http://localhost:3001/api/calendar";
+// const API_ACCOUNT = "http://localhost:3001/api/account";
 
 const API_Key = 'f47308fffabb6b503979991b847193b5';
 
